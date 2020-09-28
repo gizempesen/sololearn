f8644835ef21345500a90efe54e4de8ade4e52e5
Gizem Pesen <pesengizem999@gmail.com>
2020-09-28 15:52:54 +0300

    Create modules.py

---
+++ python/functions_modules/modules.py
@@ -0,0 +1,16 @@
+import random
+
+for i in range(5):
+   value = random.randint(1, 6)
+   print(value)
+   
+#2
+#3
+#6
+#5
+#4
+#--------------------------------
+from math import sqrt as square_root
+print(square_root(100))
+
+#10.0
