f7723dc5e677f2d96ef9c1d24468ca4612552eea
Gizem Pesen <pesengizem999@gmail.com>
2020-09-28 16:11:54 +0300

    Create exception_handling.py

---
+++ python/exceptions_files/exception_handling.py
@@ -0,0 +1,39 @@
+try:
+    num1 = 7
+    num2 = 0
+    print (num1 / num2)
+    print("Done calculation")
+except ZeroDivisionError:
+    print("An error occurred")
+    print("due to zero division")
+    
+ 
+#An error occurred
+#due to zero division 
+#------------------------------
+try:
+   variable = 10
+   print(variable + "hello")
+   print(variable / 2)
+except ZeroDivisionError:
+   print("Divided by zero")
+except (ValueError, TypeError):
+   print("Error occurred")
+   
+#Error occurred   
+#------------------------------
+#Finally
+
+try:
+   print("Hello")
+   print(1 / 0)
+except ZeroDivisionError:
+   print("Divided by zero")
+finally:
+   print("This code will run no matter what")
+   
+#Hello
+#Divided by zero
+#This code will run no matter what   
+#------------------------------
+
